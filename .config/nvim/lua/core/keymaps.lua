5146e68caf1bebfd14c99af37a484f7cb2273266
ffranksz <fraank@riseup.net>
2025-11-19 20:49:43 -0300

    Create keymaps.lua

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,68 @@
+-----------------------------------------------------
+--                     KEYMAPS                     --
+-----------------------------------------------------
+vim.g.mapleader = ' '
+local map = vim.keymap.set
+
+-- Save
+map('n','<C-s>',':w<CR>')
+
+-- Quit
+map('n','q',':q<CR>')
+
+-- Force Quit
+map('n','<C-q>',':q!<CR>')
+
+-- Undo (Ctrl+Z)
+map('n','<C-z>','u')
+
+-- Select All (Ctrl+A)
+map('n','<C-a>','ggVG')
+
+-- NERDTree
+map('n','<C-l>',':NERDTreeToggle<CR>')
+
+-- Terminal
+map('n','<leader>t',':vsplit term://zsh<CR>A')
+
+-- Dupla linha
+default_opts = { noremap=true, silent=true }
+map('n','<C-d>',':t.<CR>',default_opts)
+
+-- Comentários
+map('n','<C-/>','<Plug>NERDCommenterToggle')
+map('x','<C-/>','<Plug>NERDCommenterToggle')
+
+-- Telescope
+map('n','<F5>',':Telescope<CR>', {silent = true })
+
+-- VCoolor
+map('n','<F6>',':VCoolor<CR>')
+
+-- Carbon
+map('n','<F7>',':CarbonNow<CR>', { silent = true })
+
+-- Número e relativo
+map('n','<F2>',':set invnumber<CR>')
+map('n','<F3>',':set relativenumber!<CR>')
+
+-- PlugInstall / PlugUpdate
+vim.keymap.set("n", "<C-r>", ":PlugInstall<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-S-p>", ":PlugUpdate<CR>", { noremap = true, silent = true })
+
+-- Mason
+vim.keymap.set("n", "<C-m>", ":Mason<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", ":MasonUpdate<CR>", { noremap = true, silent = true })
+
+-- Copy to clipboard (como cp)
+vim.keymap.set('n', 'cp', '"+y', { noremap = true, silent = true })
+vim.keymap.set('v', 'cp', '"+y', { noremap = true, silent = true })
+
+-- Paste from system clipboard (c = "+p)
+vim.keymap.set('n', 'c', '"+p', {noremap = true, silent = true})
+
+-- Movimentar linhas
+map('n','<S-Up>',':m-2<CR>')
+map('n','<S-Down>',':m+1<CR>')
+map('i','<S-Up>','<Esc>:m-2<CR>')
+map('i','<S-Down>','<Esc>:m+1<CR>')
