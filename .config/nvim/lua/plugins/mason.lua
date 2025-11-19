79b27a7aa0ad6aebd360cd5d3b818f97736572bd
ffranksz <fraank@riseup.net>
2025-11-19 20:54:52 -0300

    Create mason.lua

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,7 @@
+-----------------------------------------------------
+--                     LSP + Mason                 --
+-----------------------------------------------------
+require("mason").setup{}
+require("mason-lspconfig").setup{
+  ensure_installed = {"pyright", "lua_ls"},
+}
