d055a7c22de8f15b1b46b6ea4500a4ba2e7748c3
ffranksz <fraank@riseup.net>
2025-11-19 20:44:37 -0300

    Create check_updates_click.sh

---
+++ .config/waybar/scripts/check_updates_click.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Franklin Souza
+# @ffranksz
+# Script para exibir atualizações ao clicar no módulo do Waybar
+
+echo "Atualizando repositórios..."
+sudo pacman -Syy
+
+echo
+echo "Pacotes oficiais:"
+pacman -Qu
+
+echo
+echo "Pacotes AUR:"
+paru -Qua | grep -v "^paru "
