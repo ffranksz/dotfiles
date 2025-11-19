e2207e51bd7cf1a5cb328f1823c90c668b04e527
ffranksz <fraank@riseup.net>
2025-11-19 20:50:09 -0300

    Create options.lua

---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,24 @@
+-----------------------------------------------------
+--                   BASIC SETTINGS                --
+-----------------------------------------------------
+local opt = vim.opt
+opt.number = true
+opt.relativenumber = false
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 4
+opt.cursorline = true
+opt.mouse = 'a'
+opt.termguicolors = true
+opt.clipboard = 'unnamedplus'
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.splitright = true
+opt.splitbelow = true
+opt.encoding = 'utf-8'
+opt.hidden = true
+opt.autowrite = true
+opt.swapfile = false
+opt.guifont = "JetBrainsMono Nerd Font:h12"
